cf9919c74b249caa04419146e4e0f2df2434369e
Kousican <Kousicanperumal@gmail.com>
2023-10-04 20:28:10 -0400

    First commit

---
+++ src/app/page.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import Link from "next/link";
+import NavigationFooter from "@/components/NavigationFooter";
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Restaurant Rock Star</title>
+      </Head>
+
+      <main className="min-h-screen relative overflow-hidden w-full">
+        <section className="!fixed inset-0 min-h-screen w-full bg-transparent flex flex-col items-center justify-between p-8 md:p-12 z-[20]">
+          {/* <Link href="/" className="font-semibold cursor-pointer">
+            Rock Star
+          </Link> */}
+
+          <div className="flex flex-col items-center">
+            <h2 className="text-[#FACE8D] font-dancing text-2xl sm:text-[40px] md:text-[60px] lg:text-[80px] leading-none">
+              The pure taste of
+            </h2>
+            <h1 className="font-medium text-5xl sm:text-6xl md:text-[90px] lg:text-[160px] leading-none">
+              Hotdogs
+            </h1>
+            <p className="text-xl md:text-[23px] text-center md:text-start mt-[20px] md:max-w-[600px]">
+              Never Ever Tasted!
+            </p>
+
+            <div className="w-full flex-col justify-center max-w-[650px]">
+              <div className="mt-[23px] max-w-[650px] w-full flex justify-end">
+                <div className="bg-white w-[250px] h-[163px] rounded-[10px] text-black py-4 px-6 hidden sm:block">
+                  <h3 className="font-normal tracking-wide">Opening Hours</h3>
+                  {/* <div className="mt-2 flex justify-between items-center">
+                    <h2 className="">Mon</h2>
+                    <p className="font-normal tracking-wide">7am-4:30pm</p>
+                  </div> */}
+                  <div className="mt-2 flex justify-between items-center">
+                    <h2 className="">Mon - Fri</h2>
+                    <p className="font-normal tracking-wide">7am - 4pm</p>
+                  </div>
+                  <div className="mt-2 flex justify-between items-center">
+                    <h2 className="">Sat </h2>
+                    <p className="font-normal tracking-wide">7pm - 3pm</p>
+                  </div>
+                  <div className="mt-2 flex justify-between items-center">
+                    <h2 className="">Sun </h2>
+                    <p className="font-normal tracking-wide">Closed</p>
+                  </div>
+                </div>
+              </div>
+              <NavigationFooter />
+            </div>
+          </div>
+        </section>
+
+        <div className="slider">
+          <div className="slide"></div>
+          <div className="slide"></div>
+          <div className="slide"></div>
+          <div className="slide"></div>
+          <div className="slide"></div>
+        </div>
+      </main>
+    </>
+  );
+}
---
+++ src/sections/shop/data.ts
@@ -0,0 +1,44 @@
+const data: {
+  id: number;
+  title: string;
+  description: string;
+  price: string;
+  logoBg: string;
+}[] = [
+  {
+    id: 1,
+    title: "Gift Card Light",
+    description:
+      "",
+    price: "$ 15 USD",
+    logoBg: "#5446A7",
+  },
+
+  {
+    id: 2,
+    title: "Gift Card Standard",
+    description:
+      "",
+    price: "$ 30 USD",
+    logoBg: "#981318",
+  },
+
+  {
+    id: 3,
+    title: "Gift Card Premium",
+    description:
+      "",
+    price: "$ 50 USD",
+    logoBg: "#222222",
+  },
+  {
+    id: 4,
+    title: "Gift Card Gold",
+    description:
+      "",
+    price: "$ 100 USD",
+    logoBg: "#E3C79E",
+  },
+];
+
+export default data;
