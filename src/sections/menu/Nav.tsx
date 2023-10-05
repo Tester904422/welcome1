d643e80847d5cd97d882a222f11d57e179b94ee3
Kousican <Kousicanperumal@gmail.com>
2023-10-04 23:13:43 -0400

    change-7 drinks

--- src/sections/menu/Nav.tsx
+++ src/sections/menu/Nav.tsx
@@ -11,39 +11,35 @@ interface IProps {
 //-------------------------------------------------------
 
 const Nav: FC<IProps> = ({
-  navigationHandler,
-  startersRef,
-  breakfastRef,
-  dinnerRef,
-  drinksRef,
+ 
 }) => {
   return (
-    <div className="min-h-[74px] w-full flex justify-center items-center">
-      {/* <button
-        onClick={() => navigationHandler(startersRef)}
-        className="font-light text-[#FACE8D]"
-      >
-        Starters
-      </button> */}
-      <button
-        onClick={() => navigationHandler(breakfastRef)}
-        className="font-light hover:text-[#FACE8D] text-white ml-[28px] md:ml-[48px]"
-      >
-        Breakfast / Lunch
-      </button>
-      {/* <button
-        onClick={() => navigationHandler(dinnerRef)}
-        className="font-light hover:text-[#FACE8D] text-white ml-[28px] md:ml-[48px]"
-      >
-        Dinner
-      </button> */}
-      <button
-        onClick={() => navigationHandler(drinksRef)}
-        className="font-light hover:text-[#FACE8D] text-white ml-[28px] md:ml-[48px]"
-      >
-        Drinks
-      </button>
-    </div>
+    // <div className="min-h-[74px] w-full flex justify-center items-center">
+    //   {/* <button
+    //     onClick={() => navigationHandler(startersRef)}
+    //     className="font-light text-[#FACE8D]"
+    //   >
+    //     Starters
+    //   </button> */}
+    //   <button
+    //     onClick={() => navigationHandler(breakfastRef)}
+    //     className="font-light hover:text-[#FACE8D] text-white ml-[28px] md:ml-[48px]"
+    //   >
+    //     Breakfast / Lunch
+    //   </button>
+    //   {/* <button
+    //     onClick={() => navigationHandler(dinnerRef)}
+    //     className="font-light hover:text-[#FACE8D] text-white ml-[28px] md:ml-[48px]"
+    //   >
+    //     Dinner
+    //   </button> */}
+    //   <button
+    //     onClick={() => navigationHandler(drinksRef)}
+    //     className="font-light hover:text-[#FACE8D] text-white ml-[28px] md:ml-[48px]"
+    //   >
+    //     Drinks
+    //   </button>
+    // </div>
   );
 };
 
