7c2fdadf36f17ad91e6028a140e5f437b70aab07
Kousican <Kousicanperumal@gmail.com>
2023-10-05 00:00:33 -0400

    Cahnge-12

--- src/utils/blogData.ts
+++ src/utils/blogData.ts
@@ -7,10 +7,10 @@ const blogData: {
 }[] = [
   {
     id: 1,
-    title: "Gatsby Night",
+    title: "New branch opening soon in Brunswick, ga",
     description:
       "",
-    time: "September 19, 2022",
+    time: "December, 2023",
     img: "",
   },
 
@@ -20,7 +20,7 @@ const blogData: {
     description:
       "",
     time: "September 19, 2022",
-    img: "https://plus.unsplash.com/premium_photo-1663011391833-e575d20b3a88?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80",
+    img: "",
   },
 
   {
